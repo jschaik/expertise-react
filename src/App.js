de78ff2efa31c9ab1ed218f1e6d21f7c6b30cf5f
jschaik <j.vanschaik@tcrmbo.nl>
2021-10-27 19:55:45 +0200

    back to normal

--- src/App.js
+++ src/App.js
@@ -7,25 +7,11 @@ function App() {
   const message = "dit is mijn vijfde bericht";
   const name = "Mike";
 
-  const [count, setCount] = useState(4);
-
-  const decrementCount = () => {
-    setCount(count - 1);
-  }
-
-  const incrementCount = () => {
-    setCount(count + 1);
-  }
   return (
-    // <div>
-    //   <CreateTweet />
-    //  <TweetList name={name} message={message}/>
-    // </div>
-    <> 
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
-    </>
+    <div>
+      <CreateTweet />
+     <TweetList name={name} message={message}/>
+    </div>
   );
 }
 
